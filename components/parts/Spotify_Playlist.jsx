06a80d747b54d49ee0efcf5a397c5337f8b7ebf0
Jordan Lambrecht <jordan@pixelbakery.co>
2022-02-19 15:19:34 -0600

    Made playlist name link to spotify

--- components/parts/Spotify_Playlist.jsx
+++ components/parts/Spotify_Playlist.jsx
@@ -64,13 +64,14 @@ export default function Spotify_Playlist({ playlistID }) {
             <path d='M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.669 11.538a.498.498 0 0 1-.686.165c-1.879-1.147-4.243-1.407-7.028-.77a.499.499 0 0 1-.222-.973c3.048-.696 5.662-.397 7.77.892a.5.5 0 0 1 .166.686zm.979-2.178a.624.624 0 0 1-.858.205c-2.15-1.321-5.428-1.704-7.972-.932a.625.625 0 0 1-.362-1.194c2.905-.881 6.517-.454 8.986 1.063a.624.624 0 0 1 .206.858zm.084-2.268C10.154 5.56 5.9 5.419 3.438 6.166a.748.748 0 1 1-.434-1.432c2.825-.857 7.523-.692 10.492 1.07a.747.747 0 1 1-.764 1.288z' />
           </svg> */}
           {/* <div className='text-black text-opacity-50 italic text-md'>playlist:</div> */}
-          <h2
-            color='black'
-            className='tracking-wide lg:tracking-wider align-top s font-semibold max-w-md mt-0 text-2xl sm:text-5xl md:text-6xl 2xl:text-6xl 2xl:max-w-3xl leading-none mb-2 '
-          >
-            {data.name}
-          </h2>
-
+          <Link href={data.external_urls.spotify} passHref>
+            <h2
+              color='black'
+              className='tracking-wide lg:tracking-wider align-top s font-semibold max-w-md mt-0 text-2xl sm:text-5xl md:text-6xl 2xl:text-6xl 2xl:max-w-3xl leading-none mb-2 '
+            >
+              {data.name}
+            </h2>
+          </Link>
           <p className={'text-lg  text-semibold mb-2  pb-0 mt-0 pt-0'}>
             {' '}
             {data.description ? data.description : ''}
